bd3c08c3977e3455ff738207dec9c2c0cc9c189a
Raja Ajay Kumar <68234055+RajaAjayKumar@users.noreply.github.com>
2020-09-16 23:08:06 +0530

    Add files via upload

---
+++ Coding Challenges/1 Lowercase and Uppercase.py
@@ -0,0 +1,12 @@
+# Lowercase and Uppercase
+
+def lowercase(userInput):
+    return userInput.lower()
+
+def uppercase(userInput):
+    return userInput.upper()
+
+userInput = input("Enter the Input: ")
+
+print(lowercase(userInput))
+print(uppercase(userInput))
